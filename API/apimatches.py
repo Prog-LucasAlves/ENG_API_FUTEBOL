c56b9bfe71d97f3fb395540e9ca8479636d9a818
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2024-03-14 21:11:10 -0300

    refactor: Atualização arquivo apimatches.py

---
+++ API/apimatches.py
@@ -0,0 +1,48 @@
+import requests
+
+from datetime import date, timedelta
+
+from connectdb import in_dados
+
+import os
+import dotenv
+
+PATH = '../Docker/.env'
+
+# Carrega as variáveis de ambiente do projeto
+dotenv.load_dotenv(dotenv_path=PATH, verbose=True)
+KEY = os.getenv('KEY')
+
+
+def Previousdate():
+    data_atual = date.today()
+    data_ontem = data_atual + timedelta(-1)
+    return data_ontem
+
+
+def Conectapi():
+    diaAnterior = Previousdate()
+    key = KEY
+    url = f'https://apiv3.apifootball.com/?action=get_predictions&from={diaAnterior}&to={diaAnterior}&APIkey={key}'
+    response = requests.get(url)
+    return response.json()
+
+
+def Collectdata():
+    data = Conectapi()
+    for game in data:
+        print(game['match_id'])
+        match_id = game['match_id']    
+
+        queryInsert = f"INSERT INTO infomatches VALUES ( \
+            '{match_id}')"
+
+        in_dados(queryInsert)
+    print('============================')
+    print('Dados inseridos com sucesso!')
+    print('============================')
+
+
+if __name__ == '__main__':
+    Collectdata()
+    print('Conectado com sucesso!')
